00d49d61e62e9cfdf2288f507456441f391df167
mdohrmund <31314575+mdohrmund@users.noreply.github.com>
2017-09-01 08:33:23 -0500

    Add files via upload

---
+++ Assessment/src/standard/test1.java
@@ -0,0 +1,10 @@
+package standard;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("says something funny.");
+	}
+
+}
---
+++ Assessment/src/standard/test3.java
@@ -0,0 +1,8 @@
+package standard;
+
+public class test3 {
+	  public static void main(String[] args)
+	   {
+		   System.out.println("Program comments are nonexcuting statements you add to a file for the purpose of documentations.");
+	   }
+}

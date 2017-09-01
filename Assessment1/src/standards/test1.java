87a0101feaf41ef373c0bf256cf0717606c86e9a
mdohrmund <31314575+mdohrmund@users.noreply.github.com>
2017-09-01 08:26:05 -0500

    Add files via upload

--- Assessment1/src/standards/test1.java
+++ Assessment1/src/standards/test1.java
@@ -1,5 +1,9 @@
 package standards;
 
 public class test1 {
-
+	   public static void main(String[] args)
+	   {
+		   System.out.println("Test 1.");
+		   
+	   }
 }
--- Assessment1/src/standards/test2.java
+++ Assessment1/src/standards/test2.java
@@ -1,5 +1,9 @@
 package standards;
 
 public class test2 {
+	  public static void main(String[] args)
+	   {
+		   System.out.println("Test 2.");
+	   }
 
 }
